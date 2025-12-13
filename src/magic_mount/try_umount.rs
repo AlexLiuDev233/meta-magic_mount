d7ffbe8abbf2d0d0712a9c03294ccd8f8bff2ddc
Tools-app <localhost.hutao@gmail.com>
2025-12-14 07:22:24 +0800

    feat: add hymofs hide support
    Equivalent to `ksud kernel umount`
    
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/magic_mount/try_umount.rs
+++ src/magic_mount/try_umount.rs
@@ -1,8 +1,8 @@
 use std::{
-    ffi::CString,
-    fs::{self, read_dir},
+    ffi::{CString, c_char, c_int},
+    fs::{self, OpenOptions, read_dir},
     io,
-    os::fd::RawFd,
+    os::fd::{AsRawFd, RawFd},
     path::Path,
     sync::OnceLock,
 };
@@ -21,6 +21,17 @@ const KSU_IOCTL_ADD_TRY_UMOUNT: i32 = libc::_IOW::<()>(K, 18);
 const KSU_INSTALL_MAGIC2: u32 = 0xCAFE_BABE;
 static DRIVER_FD: OnceLock<RawFd> = OnceLock::new();
 
+const HYMO_IOC_MAGIC: u32 = 0xE0;
+const HYMO_IOCTL_HIDE: u64 = ioctl_cmd_write(3, std::mem::size_of::<HymoHide>());
+const HYMO_DEV: &[&str] = &["/dev/hymo_ctl", "/proc/hymo_ctl"];
+
+#[repr(C)]
+struct HymoHide {
+    src: *const c_char,
+    target: *const c_char,
+    r#type: c_int,
+}
+
 #[repr(C)]
 struct KsuAddTryUmount {
     arg: u64,
@@ -28,6 +39,61 @@ struct KsuAddTryUmount {
     mode: u8,
 }
 
+const fn ioctl_cmd_write(nr: u32, size: usize) -> u64 {
+    let size = size as u64;
+    (1u32 << 30) as u64 | (size << 16) | ((HYMO_IOC_MAGIC as u64) << 8) | nr as u64
+}
+
+fn find_node() -> Option<RawFd> {
+    for i in HYMO_DEV {
+        if let Ok(dev) = OpenOptions::new().read(true).write(true).open(i) {
+            return Some(dev.as_raw_fd());
+        }
+    }
+
+    None
+}
+fn send_hide_hymofs<P>(target: P) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let fd = find_node();
+
+    if fd.is_none() {
+        return Ok(());
+    }
+    let fd = fd.unwrap();
+
+    let path = CString::new(target.as_ref().as_str()?)?;
+    let cmd = HymoHide {
+        src: path.as_ptr(),
+        target: std::ptr::null(),
+        r#type: 0,
+    };
+
+    let ret = unsafe {
+        #[cfg(not(target_env = "gnu"))]
+        {
+            libc::ioctl(fd, HYMO_IOCTL_HIDE as i32, &cmd)
+        }
+        #[cfg(target_env = "gnu")]
+        {
+            libc::ioctl(fd, HYMO_IOCTL_HIDE, &cmd)
+        }
+    };
+    if ret < 0 {
+        log::error!(
+            "umount {} failed: {}",
+            target.as_ref().display(),
+            io::Error::last_os_error()
+        );
+
+        return Ok(());
+    }
+
+    log::info!("umount {} successful!", target.as_ref().display());
+    Ok(())
+}
 pub fn send_unmountable<P>(target: P) -> Result<()>
 where
     P: AsRef<Path>,
@@ -59,6 +125,12 @@ where
         }
     }
 
+    for i in HYMO_DEV {
+        if fs::exists(i)? {
+            send_hide_hymofs(target.as_ref())?;
+            return Ok(());
+        }
+    }
     let path = CString::new(target.as_ref().as_str()?)?;
     let cmd = KsuAddTryUmount {
         arg: path.as_ptr() as u64,
