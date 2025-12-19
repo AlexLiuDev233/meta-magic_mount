b171b1eb9a5ae00d4f904f2b3eb7d4ab7b61baa3
Tools-app <localhost.hutao@gmail.com>
2025-12-19 20:33:15 +0800

    opt: Optimize try_umount logic
    - fixed more than warn log
    - if zn was detected for the first time, will return, instead run all try_umount logic
    
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/try_umount/mod.rs
+++ src/try_umount/mod.rs
@@ -3,16 +3,23 @@ mod kernel;
 use std::{
     fs::{self, read_dir},
     path::Path,
+    sync::atomic::AtomicBool,
 };
 
 use anyhow::Result;
 
 use crate::defs::{DISABLE_FILE_NAME, REMOVE_FILE_NAME, SKIP_MOUNT_FILE_NAME};
 
+static LAST: AtomicBool = AtomicBool::new(false);
+
 pub fn send_unmountable<P>(target: P) -> Result<()>
 where
     P: AsRef<Path>,
 {
+    if LAST.load(std::sync::atomic::Ordering::Relaxed) {
+        return Ok(());
+    }
+
     for entry in read_dir("/data/adb/modules")?.flatten() {
         let path = entry.path();
 
@@ -37,6 +44,7 @@ where
 
         if fs::read_to_string("/data/adb/zygisksu/denylist_enforce")?.trim() != "0" {
             log::warn!("zn was detected, and try_umount was cancelled.");
+            LAST.store(true, std::sync::atomic::Ordering::Relaxed);
             return Ok(());
         }
     }
